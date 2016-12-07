3e5118641366ab63c016e8f71ec56891271e1eec
Daniel Hipp <d.hipp@gmx.net>
2016-12-07 15:58:47 +0100

    css updated + javascript updated

--- MoerakiKemu/public/javascripts/main.js
+++ MoerakiKemu/public/javascripts/main.js
@@ -3,15 +3,19 @@ if (window.console) {
 }
 
 function makeBlack(){
-    $('.container').addClass('black_background');
-    $('.container').removeClass('white_background');
+    $('body').addClass('black_background');
+    $('body').removeClass('white_background');
+    $('.headline').addClass('black_background');
+    $('.headline').removeClass('white_background');
     $('#button-black').addClass('active');
     $('#button-white').removeClass('active');
 }
 
 function makeWhite(){
-    $('.container').removeClass('black_background');
-    $('.container').addClass('white_background');
+    $('body').removeClass('black_background');
+    $('body').addClass('white_background');
+    $('.headline').removeClass('black_background');
+    $('.headline').addClass('white_background');
     $('#button-black').removeClass('active');
     $('#button-white').addClass('active');
 }
