7451e5dd00ec1c847aaa619866300f71b6bd9486
Daniel Hipp <d.hipp@gmx.net>
2016-12-12 11:06:46 +0100

    changed css for the field

--- MoerakiKemu/public/javascripts/main.js
+++ MoerakiKemu/public/javascripts/main.js
@@ -1,22 +1,19 @@
 if (window.console) {
     console.log("Welcome to your Play application's JavaScript!");
-}
+};
 
-function makeBlack(){
-    $('body').addClass('black_background');
-    $('body').removeClass('white_background');
-    $('.headline').addClass('black_background');
-    $('.headline').removeClass('white_background');
-    $('#button-black').addClass('active');
-    $('#button-white').removeClass('active');
-}
-
-function makeWhite(){
+$('#button-white').click( function() {
     $('body').removeClass('black_background');
     $('body').addClass('white_background');
     $('.headline').removeClass('black_background');
     $('.headline').addClass('white_background');
-    $('#button-black').removeClass('active');
-    $('#button-white').addClass('active');
-}
+})
+
+$('#button-black').click( function() {
+    $('body').addClass('black_background');
+    $('body').removeClass('white_background');
+    $('.headline').addClass('black_background');
+    $('.headline').removeClass('white_background');
+})
+
 
