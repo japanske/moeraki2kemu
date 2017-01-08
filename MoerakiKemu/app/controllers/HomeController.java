baa0d55a750d91df60bb34d73439466f4972b449
Daniel Hipp <d.hipp@gmx.net>
2017-01-08 14:52:53 +0100

    websocket update

--- MoerakiKemu/app/controllers/HomeController.java
+++ MoerakiKemu/app/controllers/HomeController.java
@@ -1,6 +1,22 @@
 package controllers;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.concurrent.CompletionStage;
+
+import javax.inject.Singleton;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import actors.mainActor;
+import akka.stream.javadsl.Flow;
+
+import play.http.websocket.Message;
+import play.libs.F.Either;
+import play.*;
 import play.mvc.*;
+import play.mvc.Http.RequestHeader;
 import views.html.*;
 
 /**
@@ -16,9 +32,16 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
+
         return ok(index.render(""));
     }
     public Result gameHelp() {
-    	return ok(help.render());
+
+        return ok(help.render());
+    }
+    
+	public LegacyWebSocket<String> socket() {
+
+        return WebSocket.withActor(mainActor::props);
     }
 }
--- MoerakiKemu/public/javascripts/main.js
+++ MoerakiKemu/public/javascripts/main.js
@@ -1,19 +1,21 @@
 if (window.console) {
     console.log("Welcome to your Play application's JavaScript!");
-};
+}
 
-$('#button-white').click( function() {
-    $('body').removeClass('black_background');
-    $('body').addClass('white_background');
-    $('.headline').removeClass('black_background');
-    $('.headline').addClass('white_background');
-})
+$(document).ready(function (e) {
 
-$('#button-black').click( function() {
-    $('body').addClass('black_background');
-    $('body').removeClass('white_background');
-    $('.headline').addClass('black_background');
-    $('.headline').removeClass('white_background');
-})
+    $('#button-white').click( function(e) {
+        $('body').removeClass('black_background');
+        $('body').addClass('white_background');
+        $('.headline').removeClass('black_background');
+        $('.headline').addClass('white_background');
+    });
 
+    $('#button-black').click( function(e) {
+        $('body').addClass('black_background');
+        $('body').removeClass('white_background');
+        $('.headline').addClass('black_background');
+        $('.headline').removeClass('white_background');
+    });
 
+});
